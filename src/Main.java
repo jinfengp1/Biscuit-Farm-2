f834a5f87600dbddd42f2e2de80205a6a0bc3754
PanMan <146214795+jinfengp1@users.noreply.github.com>
2024-05-17 12:28:11 -0400

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -11,9 +11,11 @@ public class Main {
 }
 
 /*
+SHOP -----------------------------------------------------------
 SCREEN IS 1200x800 TOTAL
 BACKGROUNDS ARE 800x500 , FROM THE TOP (CREATE AT 0,0)
 SIDEBAR THING IS 400x800 , FROM THE RIGHT SIDE (CREATE AT 800,0)
 DIALOGUE BOX IS 800x300 , FROM THE BOTTOM (CREATE AT 0,500)
-ITEM SPRITES ARE 50x50
+PLOTS ----------------------------------------------------------
+
 */
--- src/MainFrame.java
+++ src/MainFrame.java
@@ -3,14 +3,14 @@ import javax.swing.*;
 
 
 public class MainFrame extends JFrame implements Runnable {
-    private ShopPanel p;
+    private TitleScreen p;
     private Thread windowThread;
 
     public MainFrame(String display) {
         super(display);
         int frameWidth = 1216;
         int frameHeight = 838;
-        p = new ShopPanel();
+        p = new TitleScreen();
         this.add(p);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(frameWidth, frameHeight);
