ab19e181487f77ccf0371d8abaadd6dc0c39a8ca
PanMan <146214795+jinfengp1@users.noreply.github.com>
2024-06-06 00:43:08 -0400

    Delete Shovel.java

--- Shovel.java
+++
@@ -1,9 +0,0 @@
-public class Shovel extends Item {
-
-    public double multiplier;
-
-    public Shovel(String name, String description, int cost, double mult) {
-        super(name,cost,description);
-        multiplier = mult;
-    }
-}
