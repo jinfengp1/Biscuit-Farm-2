120359c334a9a390bf7c0574a5084d3707744825
PanMan <146214795+jinfengp1@users.noreply.github.com>
2024-06-06 22:18:41 -0400

    Add files via upload

---
+++ src/Game.java
@@ -0,0 +1,48 @@
+
+import java.awt.*;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+public class Game extends JPanel {
+
+    public ArrayList<Item> inventory = new ArrayList<Item>();
+    public BufferedImage coinImage;
+    public int money;
+    public int fees;
+    public int day;
+    public final Biscuit basic = new Biscuit("Basic", "The cheap reliable food of most Biscuitown residents.", 5, 8, 1);
+
+    public Game() {
+        inventory.add(basic);
+        inventory.add(basic);
+        loadImage();
+    }
+
+    public void loadImage() {
+        try {
+            coinImage = ImageIO.read(new File("images/smalls/coin.png"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getMoney() { return money; }
+
+    public void addMoney(int m) { money += m; }
+
+    public void sellBiscuits() { // THIS METHOD WILL SELL A SELECTED ARRAY OF BISCUIT ITEMS
+        return;
+    }
+}
